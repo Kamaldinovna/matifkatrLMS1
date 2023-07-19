5f7f70cc8500ae34f3e857c84aabb81f783b2e41
Aizat <janyzakov.daniar@gmail.com>
2023-07-19 19:13:35 +0300

    Initial commit

---
+++ src/Circle.java
@@ -0,0 +1,14 @@
+public class Circle {
+    static final double p=3.14 ;
+    public static void area (double radius){
+        System.out.println(p*(radius*radius));
+
+    }
+    public static void circumference(double radius) {
+        System.out.println(p * 2 * radius);
+
+    }
+    }
+
+
+
